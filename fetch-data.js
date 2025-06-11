8c349fa3b0bb21ebb1f467587aaf06746105e360
Zeraora <54253237+ImZeraora@users.noreply.github.com>
2025-06-11 14:32:30 -0500

    Update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -4,9 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config(); // Load environment variables from .env
 
 // Informationen zu deinem Repository und Datei
-const owner = "ImpactCoding"; // GitHub Username oder Organisation
-const repo = "rr-player-database"; // Repository Name
-const path = "rr-players.json"; // Pfad zur Datei, die du ändern möchtest
+const owner = "ImZeraora"; // GitHub Username oder Organisation
+const repo = "zse-player-database"; // Repository Name
+const path = "zse-players.json"; // Pfad zur Datei, die du ändern möchtest
 const branch = "main"; // Branch, auf dem die Änderungen stattfinden sollen
 const token = process.env.ACCESS_TOKEN; // Use GitHub token from environment variables
 let sha;
@@ -39,7 +39,7 @@ async function downloadFile() {
   sha = fileData.sha;
 
   const response = await fetch(
-    "https://impactcoding.github.io/rr-player-database/rr-players.json"
+    "https://imzeraora.github.io/zse-player-database/zse-players.json"
   );
   const playerData = await response.json();
   return playerData;
@@ -59,7 +59,7 @@ async function uploadFile(fileToUpload) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      message: "Updated rr-players.json: Added new entry",
+      message: "Updated zse-players.json: Added new entry",
       content: updatedContent, // The new updated content (Base64-encoded)
       sha: sha, // SHA to overwrite the file
       branch: branch,
@@ -75,7 +75,7 @@ async function uploadFile(fileToUpload) {
 }
 
 async function fetchRooms() {
-  const response = await fetch("https://umapyoi.net/api/v1/rr-rooms");
+  const response = await fetch("http://nas.ninjawfc.com/api/groups?rk=vs_20000");
   const rooms = await response.json();
   let players = [];
 
